4cc65d6637b4f12d6b31eaa0c75d0c210051b444
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-04-17 09:37:18 +0100

    bank atm pie chart created

--- main.py
+++ main.py
@@ -31,9 +31,43 @@ print(frequencies)
 
 fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
 
+# for loop to extract key and values from frequencies append all keys to a single list and also to v=append values to a seperate list
+data_set = []
+banks = []
 for atm, frequency in frequencies.items():
-    print(atm)
-    print(frequency)
+    atm = atm
+    frequency = int(frequency)
+    data_set.append(frequency)
+    banks.append(atm)
 
-data = [(item.split()) for item in frequencies]
-print(data)
+
+data = data_set  # list of frequency of bank vists
+bank_atm = banks  # list of bank atms
+
+
+def func(pct, allvals):
+    absolute = int(np.round(pct / 100.0 * np.sum(allvals)))
+    return "{:.1f}%\n({:d})".format(pct, absolute)
+
+
+wedges, texts, autotexts = ax.pie(
+    data, autopct=lambda pct: func(pct, data), textprops=dict(color="w")
+)
+
+ax.legend(
+    wedges,
+    bank_atm,
+    title="Bank ATM",
+    loc="center left",
+    bbox_to_anchor=(1, 0, 0.5, 1),
+)
+
+plt.setp(autotexts, size=8, weight="bold")
+
+ax.set_title("The most used bank ATM")
+
+# plt.show()
+
+plt.savefig(
+    "bank-atm_pie-chart.png", bbox_inches="tight"
+)  # first arguement is the name of the file that I want to create and have my graph saved in. Second arguement is used to trim extra whitespaces from the plot

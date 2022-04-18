12216389b23ea25cf05846cb886490229f439e2b
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-04-18 12:33:41 +0100

    card withdrawals class created

---
+++ CardWithdrawQuery.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from CsvReader import CsvReader
+
+
+class CardWithdrawQuery:
+    def __init__(self, csv_file):
+        self.reader = CsvReader(csv_file)
+
+    def get_means(self):
+        xyz_data = []
+        other_data = []
+        for row in self.reader.read():
+            data1 = int(row[3])
+            xyz_data.append(data1)
+            data2 = int(row[4])
+            other_data.append(data2)
+
+        xyz_mean = round(sum(xyz_data) / len(xyz_data))
+        other_mean = round(sum(other_data) / len(other_data))
+
+        return xyz_mean, other_mean
+
+    def run(self):
+        fig, ax = plt.subplots()
+
+        data = self.get_means()[0]  # average amout of xyz card withdrawals
+        data2 = self.get_means()[1]  # average amout of other card withdrawals
+
+        labels = ["Average number of card withdrawals per card type"]  # x axis label
+        xyz_means = data
+        other_means = data2
+
+        x = np.arange(len(labels))  # the label locations
+        width = 0.35  # the width of the bars
+
+        fig, ax = plt.subplots()
+        xyz_bar = ax.bar(
+            x - width / 1, xyz_means, width, label="No Of XYZ Card Withdrawals"
+        )
+        other_bar = ax.bar(
+            x + width / 1, other_means, width, label="No Of Other Card Withdrawals"
+        )
+
+        # Add some text for labels, title and custom x-axis tick labels, etc.
+        ax.set_ylabel("Percentage scores (%)")
+        ax.set_title("Number of card withdrawals comparison")
+        ax.set_xticks(x, labels)
+        ax.legend()
+        ax.bar_label(xyz_bar, padding=3)
+        ax.bar_label(other_bar, padding=3)
+
+        fig.tight_layout()
+
+        plt.show()
+
+        # plt.savefig(
+        #     "card_withdrawals-bar_chart.png", bbox_inches="tight"
+        # )  # first arguement is the name of the file that I want to create and have my graph saved in. Second arguement is used to trim extra whitespaces from the plot
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from BankFrequencyQuery import BankFrequencyQuery
+from CardWithdrawQuery import CardWithdrawQuery
 from DayFrequencyQuery import DayFrequencyQuery
 
 # if I have a CSV file without a header then reader.read(header=False)
@@ -7,3 +8,6 @@ bank_frequency_query.run()
 
 day_frequency_query = DayFrequencyQuery("AggregatedData.csv")
 day_frequency_query.run()
+
+card_withdraw_query = CardWithdrawQuery("AggregatedData.csv")
+card_withdraw_query.run()

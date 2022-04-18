6e457f585209ff44404469e72f73f248b595c1a3
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-04-18 01:50:11 +0100

    week day frequency class complete

--- DayFrequencyQuery.py
+++ DayFrequencyQuery.py
@@ -4,20 +4,19 @@ import numpy as np
 from CsvReader import CsvReader
 
 
-class BankFrequencyQuery:
+class DayFrequencyQuery:
     def __init__(self, csv_file):
         self.reader = CsvReader(csv_file)
 
     def get_frequencies(self):
-        atms = [row[0] for row in self.reader.read()]
-
-        # for loop that calculates the frequency of how often each bank atm is used
+        days = [row[8].title() for row in self.reader.read()]
+        # for loop that calculates the frequented week day to bank atm
         frequencies = {}
-        for atm in atms:
-            if atm in frequencies:
-                frequencies[atm] += 1
+        for day in days:
+            if day in frequencies:
+                frequencies[day] += 1
             else:
-                frequencies[atm] = 1
+                frequencies[day] = 1
 
         return frequencies
 
@@ -41,17 +40,17 @@ class BankFrequencyQuery:
         ax.legend(
             wedges,
             bank_atms,
-            title="Bank ATMs",
+            title="Week Days",
             loc="center left",
             bbox_to_anchor=(1, 0, 0.5, 1),
         )
 
-        plt.setp(autotexts, size=8, weight="bold")
+        plt.setp(autotexts, size=6, weight="bold")
 
-        ax.set_title("The most used bank ATM")
+        ax.set_title("Frequency of bank ATM vists per week day")
 
         plt.show()
 
         # plt.savefig(
-        #     "bank-atm_pie-chart.png", bbox_inches="tight"
+        #     "week_day_pie-chart.png", bbox_inches="tight"
         # )  # first arguement is the name of the file that I want to create and have my graph saved in. Second arguement is used to trim extra whitespaces from the plot
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from BankFrequencyQuery import BankFrequencyQuery
+from DayFrequencyQuery import DayFrequencyQuery
 
 # if I have a CSV file without a header then reader.read(header=False)
-frequency_query = BankFrequencyQuery("AggregatedData.csv")
-frequency_query.run()
+bank_frequency_query = BankFrequencyQuery("AggregatedData.csv")
+bank_frequency_query.run()
+
+day_frequency_query = DayFrequencyQuery("AggregatedData.csv")
+day_frequency_query.run()
